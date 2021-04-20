e6ff6240e80fbaece65b1dcdc4c6a1f7e358ab84
MauroLaBarbera <you@example.com>
2021-04-20 16:39:08 +0200

    bonus if/else

--- js/main.js
+++ js/main.js
@@ -12,7 +12,14 @@ const app = new Vue({
     },
     methods: {
         change: function () {
-            this.center = this.center === 'text' ? 'red' : 'text';
+            //this.center = this.center === 'text' ? 'red' : 'text';
+            if (this.center === 'text') {
+                this.center = 'red';
+                this.img = 'https://picsum.photos/300';
+            } else {
+                this.center = 'text';
+                this.img = 'https://picsum.photos/200'
+            }
         }
     }
 });
