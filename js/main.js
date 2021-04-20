58fa8aa5a8124dbdb5b9e028123af1dd283619b4
MauroLaBarbera <you@example.com>
2021-04-20 16:30:06 +0200

    finisch

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+ * Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
+
+const app = new Vue({
+    el: '#header',
+    data: {
+        title: 'Lorem Picsum',
+        img: 'https://picsum.photos/200',
+        center: 'text'
+    },
+});
